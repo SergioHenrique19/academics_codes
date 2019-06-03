1aa83b23393dbbea9e3ae1fb0e1d6273bfd7aac5
SergioHenrique19 <garciasergiodm@gmail.com>
2019-06-03 15:22:16 -0300

    Exemplos de codigos em Haskell

--- ufla_201901/plp/paradigma_funcional/fatorial.hs
+++ ufla_201901/plp/paradigma_funcional/fatorial.hs
@@ -1,8 +1,10 @@
-fac :: Integer -> Integer
-fac 0 = 1
-fac n = n * fac (n - 1)
+fat :: Integer -> Integer
+fat n
+    | n == 0 = 1
+    | n > 0 = n * fat (n - 1)
+    | otherwise = error "indefinido..."
 
 main = do
     putStr "Fatorial: "
     n <- readLn
-    print (fac n)
+    print (fat n)
--- ufla_201901/plp/paradigma_funcional/fibonacci.hs
+++ ufla_201901/plp/paradigma_funcional/fibonacci.hs
@@ -1,7 +1,9 @@
 fib :: Integer -> Integer
-fib 0 = 0
-fib 1 = 1
-fib n = fib (n - 2) + fib (n - 1)
+fib n
+    | n == 0 = 0
+    | n == 1 = 1
+    | n > 1 = fib (n - 2) + fib (n - 1)
+    | otherwise = error "indefinido..."
 
 main = do
     putStr "Fibonacci: "
