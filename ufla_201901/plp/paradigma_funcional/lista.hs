dd498c22f0d1fea94c5633a60c5818938cdcf091
SergioHenrique19 <garciasergiodm@gmail.com>
2019-06-05 13:47:59 -0300

    Adicionado codigos em Haskell

--- ufla_201901/plp/paradigma_funcional/lista.hs
+++ ufla_201901/plp/paradigma_funcional/lista.hs
@@ -28,3 +28,41 @@ inver_lista l = inver_aux l []
 inv_list_ot :: [t] -> [t]
 inv_list_ot [] = []
 inv_list_ot (a:b) = inv_list_ot b ++ [a]
+
+-- funcao que retorna se elemento pertence a lista
+pertence :: [Int] -> Int -> Bool
+pertence [] _ = False
+pertence (a:b) n
+    | (a == n) = True
+    | otherwise = pertence b n
+
+-- funcao que retorna maior elemento da lista
+maior :: [Int] -> Int
+maior [a] = a
+maior (a:b)
+    | (a > maior b) = a
+    | otherwise = maior b
+
+-- funcao que verifica se todos os elementos sao pares
+todos_pares :: [Int] -> Bool
+todos_pares [] = True
+todos_pares (a:b)
+    | (mod a 2) /= 0 = False
+    | otherwise = todos_pares b
+
+-- controe lista com numeros pares de 1 .. n
+par :: Int -> Bool
+par x = mod x 2 == 0
+
+lista = [x | x <- [1 .. 100], par x]
+
+-- construtor de tupla
+tupla = [(x,y) | x <- [1..5], y <- [6..10]]
+
+{- funcao zip: junta duas listas formando uma tupla
+zip :: [a] -> [b] -> [(a,b)] -}
+
+-- funcao que inverte lista
+inverter :: [a] -> [a]
+inverter [] = []
+inverter (a:b) = (inverter b)++[a]
