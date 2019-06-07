a0bfb65a094563c7df40dcc8579f1a136f1aecdf
SergioHenrique19 <garciasergiodm@gmail.com>
2019-06-07 11:34:11 -0300

    Adicionado respostas das questoes 5, 6, 7 e 8

--- ufla_201901/intro_sbd/comandos_sql/exerc07-scripts-respostas.sql
+++ ufla_201901/intro_sbd/comandos_sql/exerc07-scripts-respostas.sql
@@ -4,15 +4,36 @@
 use Empresa;
 
 /* [1] Selecione o endereço e o salario do funcionário de nome ‘Luciana S. Santos’ */
-SELECT endereco, salario FROM Funcionario WHERE nomeFunc='Luciana S. Santos';
+SELECT endereco, salario FROM Funcionario
+WHERE nomeFunc='Luciana S. Santos';
 
 /* [2] Selecione o nome e o salário dos funcionários que nasceram  entre os anos de 1960 e 1969, inclusive, do sexo feminino e que ganham menos de 1000 */
-SELECT nomeFunc, salario FROM Funcionario WHERE dataNasc between "1960-01-01" and "1969-12-31" and sexo = 'F' and salario < 1000;
+SELECT nomeFunc, salario FROM Funcionario
+WHERE dataNasc BETWEEN "1960-01-01" AND "1969-12-31" AND sexo = 'F' AND salario < 1000;
 
 /* [3] Selecione o nome dos dependentes do funcionário de nome ‘João B. Silva’ */
-SELECT nomeDep FROM Dependente join Funcionario on Funcionario.idFunc = Dependente.idFunc WHERE Funcionario.nomeFunc = 'João B. Silva';
+SELECT nomeDep FROM Dependente JOIN Funcionario ON Funcionario.idFunc = Dependente.idFunc
+WHERE Funcionario.nomeFunc = 'João B. Silva';
 
 /* [4] Selecione o nome dos projetos que o funcionário de nome ‘Frank T. Santos’ trabalha */
-SELECT nomeProj FROM Projeto natural join Trabalha join Funcionario on Funcionario.idFunc = Trabalha.idFunc WHERE Funcionario.nomeFunc = 'Frank T. Santos';
+SELECT nomeProj FROM Projeto NATURAL JOIN Trabalha JOIN Funcionario ON Funcionario.idFunc = Trabalha.idFunc
+WHERE Funcionario.nomeFunc = 'Frank T. Santos';
 
 /* [5] Selecione o nome dos funcionários que trabalham em projetos controlados pelo departamento de nome ‘ Construção’ */
+SELECT nomeFunc FROM Funcionario F, Trabalha T, Projeto P, Departamento D
+WHERE nomeDepto = 'Construção' AND D.idDepto=P.idDepto AND P.idProj=T.idProj AND T.idFunc=F.idFunc;
+
+/* [6] Selecione o nome dos funcionários supervisionados pelo funcionário de nome ‘Frank T. Santos’. */
+SELECT nomeFunc FROM Funcionario
+WHERE idSuperv = (SELECT idFunc FROM Funcionario WHERE nomeFunc = 'Frank T. Santos');
+
+/* [7] Selecione o nome e endereço dos funcionários que não tem nenhum dependente */
+SELECT nomeFunc, endereco FROM Funcionario
+WHERE idFunc NOT IN (SELECT idFunc FROM Dependente);
+
+/* [8] Selecione o nome dos funcionários que trabalham no departamento de nome ‘Pesquisa’ ou que trabalham no projeto de nome ‘N. Benefícios’ */
+SELECT nomeFunc FROM Funcionario NATURAL JOIN Departamento WHERE nomeDepto = 'Pesquisa'
+UNION
+SELECT nomeFunc FROM Funcionario JOIN 
+(Trabalha NATURAL JOIN Projeto) ON Funcionario.idFunc = Trabalha.idFunc
+WHERE Projeto.nomeProj = 'N. Benefícios';
