d89a73d99f7fbeec4a6aeaccb3781c5f5a9f5a11
SergioHenrique19 <garciasergiodm@gmail.com>
2019-07-05 00:08:54 -0300

    Adicionado funcoes da lista implementadas

--- ufla_201901/plp/paradigma_funcional/lista_operacoes.hs
+++ ufla_201901/plp/paradigma_funcional/lista_operacoes.hs
@@ -30,3 +30,78 @@ ultimo :: [Int] -> Int
 ultimo (h:t)
     | length (h:t) == 1 = h
     | otherwise = ultimo t
+
+-- retorna primeiros elementos da lista
+primeiros :: [Int] -> [Int]
+primeiros (h:t)
+    | t == [] = []
+    | otherwise = h : primeiros t
+
+-- retorna valor em indice da lista
+indice :: [Int] -> Int -> Int
+indice (h:t) 0 = h
+indice (h:t) n = indice t (n-1)
+
+-- verifica se valor existe na lista
+elemento :: [Int] -> Int -> Bool
+elemento [] _ = False
+elemento (h:t) n
+    | h == n = True
+    | otherwise = elemento t n
+
+-- retorna tamanho da lista
+tamanho :: [Int] -> Int
+tamanho [] = 0
+tamanho (h:t) = 1 + tamanho t
+
+-- verifica se lista vazia
+vazio :: [Int] -> Bool
+vazio [] = True
+vazio _ = False
+
+-- retorna lista inversa
+inverter :: [Int] -> [Int]
+inverter [] = []
+inverter (h:t) = inverter t ++ [h]
+
+-- retorna n elementos da lista em outra lista
+pegar :: [Int] -> Int -> [Int]
+pegar [] _ = []
+pegar (h:t) 0 = []
+pegar (h:t) n = h : pegar t (n-1)
+
+-- retorna lista com n primeiros elementos excluidos
+vazar :: [Int] -> Int -> [Int]
+vazar [] _ = []
+vazar (h:t) 0 = (h:t)
+vazar (h:t) n = vazar t (n-1)
+
+-- retorna menor valor da lista
+menorzin :: [Int] -> Int
+menorzin (h:t) = menorzinAux t h
+
+menorzinAux :: [Int] -> Int -> Int
+menorzinAux [] l = l
+menorzinAux (h:t) l
+    | h < l = menorzinAux t h
+    | otherwise = menorzinAux t l
+
+-- retorna maior valor da lista
+maiorzin :: [Int] -> Int
+maiorzin (h:t) = maiorzinAux t h
+
+maiorzinAux :: [Int] -> Int -> Int
+maiorzinAux [] m = m
+maiorzinAux (h:t) m
+    | h > m = maiorzinAux t h
+    | otherwise = maiorzinAux t m
+
+-- retorna somatorio da lista
+somador :: [Int] -> Int
+somador [] = 0
+somador (h:t) = h + somador t
+
+-- retorna produto da lista
+produto :: [Int] -> Int
+produto [] = 1
+produto (h:t) = h * produto t
