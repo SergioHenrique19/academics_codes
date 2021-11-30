b3086c844965aadd62f1cdf131e4a4f69fe2245e
luizdopc <luizotaviosoares@outlook.com>
2021-11-30 20:30:05 -0300

    finish 2

--- ufla_202101/arq02/trabalho_pratico_02/questao02.c
+++ ufla_202101/arq02/trabalho_pratico_02/questao02.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 int main() {
+    printf("Type a double scalar: ");
+    double scalar;
+    scanf("%lf", &scalar);
+
     double* x = (double*)aligned_alloc(32, 64 * sizeof(double));
     x[0] = 1.0;
     x[1] = 1.0;
     x[2] = 1.0;
     x[3] = 1.0;
 
-    printf("Type a double scalar: ");
-    double scalar;
-    scanf("%lf", &scalar);
-
-    for(int i=0; i<4; i++){
-        x[i] *= scalar;
-    }
-
     __m256d XfromMemory = _mm256_load_pd(x);
-    
+
+    __m256d scalarArray = _mm256_set1_pd(scalar);
+
+    __m256d resultMul = _mm256_mul_pd(XfromMemory, scalarArray);
+
     double* y = (double*)aligned_alloc(32, 64 * sizeof(double));
     y[0] = 1.0;
     y[1] = 1.0;
@@ -26,7 +26,7 @@ int main() {
 
     __m256d YfromMemory = _mm256_load_pd(y);
     
-    __m256d result = _mm256_add_pd(XfromMemory, YfromMemory);
+    __m256d result = _mm256_add_pd(resultMul, YfromMemory);
 
     double* resultAddr = (double*)aligned_alloc(32, 64 * sizeof(double));
 
